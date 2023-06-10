b02fb3caf90b35c536b1df52cbade43eb24bf6b5
Alvin Wo <haosuwo@gmail.com>
2023-06-10 16:19:03 +0800

    implement convernt length

--- LenghConversion/LenghConversion/ContentView.swift
+++ LenghConversion/LenghConversion/ContentView.swift
@@ -8,18 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    private let units = ["meters", "kilometers", "feet", "yards", "miles"]
-    @State private var sourceUnit= "meters"
-    @State private var destUnit = "kilometers"
-    
+    private let units: [UnitLength] = [.meters, .miles, .kilometers, .feet, .yards, .miles, .inches]
+    @State private var sourceIndex: Int = 3
+    @State private var destIndex: Int = 4
+    @State private var sourceValue: Double = 0.0
+    private let unitFormatter = MeasurementFormatter()
+
+    private var outputValue: Double {
+        let source = Measurement(value: sourceValue, unit: units[sourceIndex])
+        let output = source.converted(to: units[destIndex])
+        return output.value
+    }
+
+    // Define unitPicker as a closure
+    private let unitPicker: () -> ForEach<Range<Int>, Int, Text>
+
+    init() {
+        unitFormatter.unitStyle = .long
+        // Capture units in a local constant
+        let units = self.units
+        let formatter = unitFormatter
+        unitPicker = {
+            ForEach(0 ..< units.count, id: \.self) { index in
+                Text(formatter.string(from: Measurement(value: 0, unit: units[index]).unit))
+            }
+        }
+    }
+
     var body: some View {
-        NavigationView {
+        return NavigationView {
             Form {
                 Section {
-                    Picker("Source Unit", sources: units, selection: $sourceUnit) {
-                        
-                    }
+                    Picker("Source Unit", selection: $sourceIndex, content: unitPicker)
+                    Picker("Target Unit", selection: $destIndex, content: unitPicker)
+                }
+
+                Section {
+                    TextField("Enter the value", text: Binding(
+                        get: { String(self.sourceValue) },
+                        set: {
+                            self.sourceValue = Double($0) ?? 0.0
+                        }
+                    )).keyboardType(.decimalPad)
+                } header: {
+                    Text("Input")
+                }
+
+                Section {
+                    Text(String(format: "%.2f", outputValue))
+                } header: {
+                    Text("Output")
                 }
             }.navigationTitle("Length Conversion")
         }
