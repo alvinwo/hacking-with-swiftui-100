6997ad785e17bca1ef3f42dfed1aed7f4017a4f8
Alvin Wo <haosuwo@gmail.com>
2023-06-12 22:47:22 +0800

    function ready

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alvin Wo on 2023/6/12.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
