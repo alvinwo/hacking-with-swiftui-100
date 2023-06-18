30d396e5757ccaf2fda7d17ade5637456b7a39ef
Alvin Wo <haosuwo@gmail.com>
2023-06-19 00:28:40 +0800

    project 4

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alvin Wo on 2023/6/16.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
