5dc4a05cdfdab6f6845be3ccad4cf9e76e3e2763
Alvin Wo <haosuwo@gmail.com>
2023-06-19 23:56:07 +0800

    project 4

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alvin Wo on 2023/6/19.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
